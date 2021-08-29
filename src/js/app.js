5450ce9eb39467270e69f4bf678b18b1af47a21c
Rafał Kociatkiewicz <rafal.kociatkiewicz@gmail.com>
2021-08-29 17:44:49 +0200

    Added splide package

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 import '../scss/app.scss';
+//import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+// import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
-/* Your JS Code goes here */
+import Splide from "@splidejs/splide";
+
+new Splide('#splide').mount();
