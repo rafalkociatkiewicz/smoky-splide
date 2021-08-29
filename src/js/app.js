18d24caa8ff96941150c119f4dcd32b7e8cc0e05
Rafał Kociatkiewicz <rafal.kociatkiewicz@gmail.com>
2021-08-29 20:19:55 +0200

    Simple version of clouds animated in CSS

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
+//import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 import '../scss/app.scss';
-//import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 // import '@splidejs/splide/dist/css/themes/splide-sea-green.min.css';
-import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+// import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
 
 import Splide from "@splidejs/splide";
 
-new Splide('#splide').mount();
+new Splide('#splide',{
+    type: 'loop',
+    perPage: 1
+}).mount();
